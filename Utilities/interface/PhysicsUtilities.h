a7141a5afda1b4bc0b822e26978cd0c46e8dc5b9
Nick McColl <nickmccoll@gmail.com>
2017-07-07 09:42:30 -0700

    Remove compiler warning

--- Utilities/interface/PhysicsUtilities.h
+++ Utilities/interface/PhysicsUtilities.h
@@ -268,11 +268,6 @@ void trash(std::vector<Object*>& objects);
 template<typename Key, typename Object>
 void trash(std::map<Key,Object*>& objects);
 
-//_____________________________________________________________________________
-// helper functions
-//_____________________________________________________________________________
-template<typename Obj>
-inline bool alwaysTrue(const Obj& obj) {return true;}
 
 //_____________________________________________________________________________
 // Filters a collection into a vector of pointers
