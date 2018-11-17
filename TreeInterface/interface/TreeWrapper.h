0446ce5e9a628e3491a9bcc8b28fc9cac666a3cc
Nick McColl <nickmccoll@gmail.com>
2018-11-17 12:31:23 -0800

    Array support, plus we now return branches

--- TreeInterface/interface/TreeWrapper.h
+++ TreeInterface/interface/TreeWrapper.h
@@ -25,26 +25,31 @@
 
       ~TreeWrapper();
 
-      Int_t		getBufSize()		  const { return fBufSize;	}
-      Int_t		getSplitLevel()		  const { return fSplitLevel;	}
-      TString		getTreeName()		  const { return fTreeName;	}
-      void		setBufSize(Int_t b	)	{ fBufSize = b;		}
-      void		setSplitLevel(Int_t s	)	{ fSplitLevel = s;	}
-      void		setTreeName(TString n	)	{ fTreeName = n;	fTree->SetName(fTreeName.Data());	}
-      void		fill()				{ fTree->Fill();	}
-      TTree		*getTree()			{ return fTree;		}
+      Int_t     getBufSize()          const { return fBufSize;  }
+      Int_t     getSplitLevel()       const { return fSplitLevel;   }
+      TString       getTreeName()         const { return fTreeName; }
+      void      setBufSize(Int_t b  )   { fBufSize = b;     }
+      void      setSplitLevel(Int_t s   )   { fSplitLevel = s;  }
+      void      setTreeName(TString n   )   { fTreeName = n;    fTree->SetName(fTreeName.Data());   }
+      void      fill()              { fTree->Fill();    }
+      TTree     *getTree()          { return fTree;     }
 
       template<class T>
-      void		book(const char *name, T& var, const char *type)	{ fTree->Branch(name, &var, TString(name).Append("/").Append(type).Data()); }
+      TBranch*      book(const char *name, T& var, const char *type)    { return fTree->Branch(name, &var, TString(name).Append("/").Append(type).Data()); }
 
       template<class T>
-      void		book(const char *name, std::vector<T>& varv)		{ fTree->Branch(name, &varv);	}
+      TBranch*      book(const char *name, T& varv)        { return fTree->Branch(name, &varv);   }
+
+      template<class T>
+      TBranch*      bookArray(const char *name, T* var, const char *type, const char *sizeName)    {
+          return fTree->Branch(name, var, (TString(name) +"["+sizeName+"]/"+type ).Data());
+      }
 
     protected :
-      Int_t		fBufSize;
-      Int_t		fSplitLevel;
-      TString		fTreeName;
-      TTree		*fTree;
+      Int_t     fBufSize;
+      Int_t     fSplitLevel;
+      TString       fTreeName;
+      TTree     *fTree;
 
   }; // TreeWrapper
 
