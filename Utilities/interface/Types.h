ca86c61cf9b7989618fb1d211c63f5fa8f954c03
Nick McColl <nickmccoll@gmail.com>
2018-11-17 12:30:58 -0800

    Shared ptr vector helper typedefs

--- Utilities/interface/Types.h
+++ Utilities/interface/Types.h
@@ -18,6 +18,26 @@ typedef   unsigned short int     size16; //16 bit 0->65,536
 typedef   unsigned int           size  ; //32 bit 0->4,294,967,296
 typedef   unsigned long long     size64; //64 bit 0->18,446,744,073,709,551,615
 
+typedef   std::shared_ptr<std::vector<double>>  spvdouble  ;
+typedef   std::shared_ptr<std::vector<float >>  spvfloat   ;
+typedef   std::shared_ptr<std::vector<int   >>  spvint     ;
+typedef   std::shared_ptr<std::vector<int8  >>  spvint8    ;
+typedef   std::shared_ptr<std::vector<int16 >>  spvint16   ;
+typedef   std::shared_ptr<std::vector<size8 >>  spvsize8   ;
+typedef   std::shared_ptr<std::vector<size16>>  spvsize16  ;
+typedef   std::shared_ptr<std::vector<size  >>  spvsize    ;
+typedef   std::shared_ptr<std::vector<size64>>  spvsize64  ;
+
+inline spvdouble  make_spvdouble() {return std::make_shared<std::vector<double>>();}
+inline spvfloat   make_spvfloat () {return std::make_shared<std::vector<float >>();}
+inline spvint     make_spvint   () {return std::make_shared<std::vector<int   >>();}
+inline spvint8    make_spvint8  () {return std::make_shared<std::vector<int8  >>();}
+inline spvint16   make_spvint16 () {return std::make_shared<std::vector<int16 >>();}
+inline spvsize8   make_spvsize8 () {return std::make_shared<std::vector<size8 >>();}
+inline spvsize16  make_spvsize16() {return std::make_shared<std::vector<size16>>();}
+inline spvsize    make_spvsize  () {return std::make_shared<std::vector<size  >>();}
+inline spvsize64  make_spvsize64() {return std::make_shared<std::vector<size64>>();}
+
 
 typedef ROOT::Math::PtEtaPhiM4D<double>				                CylLorentzCoord;
 typedef ROOT::Math::PtEtaPhiM4D<float> 				                CylLorentzCoordF;
